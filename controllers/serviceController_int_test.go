e64d560ba476e1a4a8d41f94b3e71692209d4399
Niklas <niklas.hinze@cloudogu.com>
2022-12-01 10:58:55 +0100

    Reset wait tickrate in integration test. Throw error in integration test if the crd path is missing. Update dogu operator dependency #14.

--- controllers/serviceController_int_test.go
+++ controllers/serviceController_int_test.go
@@ -193,7 +193,7 @@ var _ = Describe("Creating ingress objects with the ingress generator", func() {
 			client, err := kubernetes.NewForConfig(ctrl.GetConfigOrDie())
 			Expect(err).NotTo(HaveOccurred())
 			deploymentWaiter := dogustart.NewDeploymentReadyChecker(client, "my-test-namespace")
-			waitOptions := dogustart.WaitOptions{Timeout: 5, TickRate: 3}
+			waitOptions := dogustart.WaitOptions{Timeout: 5, TickRate: time.Millisecond * 200}
 			err = deploymentWaiter.WaitForReady(ctx, "nexus", waitOptions, func(ctx context.Context) {})
 			Expect(err).NotTo(HaveOccurred())
 
