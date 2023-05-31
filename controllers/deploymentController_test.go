3c767f4c591c7d5ce761bea1c22ead84848fe438
meiserloh <matthias.eiserloh@cloudogu.com>
2023-05-31 09:45:34 +0200

    #30: Add tests for deploymentController constructor and getDeployment

---
+++ controllers/deploymentController_test.go
@@ -0,0 +1,62 @@
+package controllers
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	testclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"testing"
+)
+
+func TestNewDeploymentReconciler(t *testing.T) {
+	t.Run("successfully create deployment reconciler", func(t *testing.T) {
+		// given
+		clientMock := testclient.NewClientBuilder().WithScheme(getScheme()).Build()
+		ingressUpdaterMock := NewMockIngressUpdater(t)
+
+		// when
+		reconciler := NewDeploymentReconciler(clientMock, ingressUpdaterMock)
+
+		// then
+		assert.NotNil(t, reconciler)
+		assert.NotNil(t, reconciler.client)
+		assert.NotNil(t, reconciler.updater)
+	})
+}
+
+func TestDeploymentReconciler_getDeployment(t *testing.T) {
+	name := "my-app"
+	namespace := "myNamespace"
+	t.Run("successfully get deployment", func(t *testing.T) {
+		// given
+		deployment := &appsv1.Deployment{ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: namespace}}
+		clientMock := testclient.NewClientBuilder().WithScheme(getScheme()).WithObjects(deployment).Build()
+
+		reconciler := NewDeploymentReconciler(clientMock, nil)
+
+		// when
+		result, err := reconciler.getDeployment(testCtx,
+			ctrl.Request{NamespacedName: types.NamespacedName{Namespace: namespace, Name: name}})
+		// then
+		require.NoError(t, err)
+		assert.Equal(t, name, result.Name)
+		assert.Equal(t, namespace, result.Namespace)
+	})
+
+	t.Run("failed to get deployment if no deployment has been created", func(t *testing.T) {
+		// given
+		clientMock := testclient.NewClientBuilder().WithScheme(getScheme()).Build()
+
+		reconciler := NewDeploymentReconciler(clientMock, nil)
+
+		// when
+		result, err := reconciler.getDeployment(testCtx,
+			ctrl.Request{NamespacedName: types.NamespacedName{Namespace: namespace, Name: name}})
+		// then
+		require.ErrorContains(t, err, "failed to get deployment: deployments.apps \"my-app\" not found")
+		require.Nil(t, result)
+	})
+}
