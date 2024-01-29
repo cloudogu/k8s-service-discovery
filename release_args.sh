b3a2688385ef6022f1a1e40c102920425b48ed24
Simon Klein <simon.klein@cloudogu.com>
2024-01-29 15:49:15 +0100

    Update release_args

--- release_args.sh
+++ release_args.sh
@@ -9,8 +9,8 @@ update_versions_modify_files() {
   valuesYAML=k8s/helm/values.yaml
   componentPatchTplYAML=k8s/helm/component-patch-tpl.yaml
 
-  yq -i ".manager.image.tag = \"${newReleaseVersion}\"" "${valuesYAML}"
-  yq -i ".values.images.serviceDiscovery |= sub(\":(([0-9]+)\.([0-9]+)\.([0-9]+)((?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))|(?:\+[0-9A-Za-z-]+))?)\", \":${newReleaseVersion}\")" "${componentPatchTplYAML}"
+  yq -oy -i ".manager.image.tag = \"${newReleaseVersion}\"" "${valuesYAML}"
+  yq -oy -i ".values.images.serviceDiscovery |= sub(\":(([0-9]+)\.([0-9]+)\.([0-9]+)((?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))|(?:\+[0-9A-Za-z-]+))?)\", \":${newReleaseVersion}\")" "${componentPatchTplYAML}"
 }
 
 update_versions_stage_modified_files() {
