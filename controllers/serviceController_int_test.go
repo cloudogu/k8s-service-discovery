e9a5034ccca39b2773ac92e8281c23d44a7e8c90
Simon Klein <simon.klein@cloudogu.com>
2025-01-20 09:48:04 +0100

    #78 Add missing expect calls

--- controllers/serviceController_int_test.go
+++ controllers/serviceController_int_test.go
@@ -236,7 +236,7 @@ var _ = Describe("Creating ingress objects with the ingress generator", func() {
 			Expect(expectedIngress.Items[1].Namespace).Should(Equal(myNamespace))
 			Expect(expectedIngress.Items[1].Name).Should(Equal("nexus-docker-registry"))
 			Expect(*expectedIngress.Items[1].Spec.IngressClassName).Should(Equal(myIngressClassName))
-			Expect(expectedIngress.Items[1].Spec.Rules[0].HTTP.Paths[0].Path).Should(Equal("/v2"))
+			Expect(expectedIngress.Items[1].Spec.Rules[0].HTTP.Paths[0].Path).Should(Equal("/v2(/|$)(.*)"))
 			Expect(*expectedIngress.Items[1].Spec.Rules[0].HTTP.Paths[0].PathType).Should(Equal(networking.PathTypePrefix))
 			Expect(expectedIngress.Items[1].Spec.Rules[0].HTTP.Paths[0].Backend.Service.Name).Should(Equal("nexus"))
 			Expect(expectedIngress.Items[1].Spec.Rules[0].HTTP.Paths[0].Backend.Service.Port.Number).Should(Equal(int32(8082)))
