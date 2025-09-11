213d0028e5ab5f886da6443227d440f917e172bb
Nico Franzeck <nico.franzeck@cloudogu.com>
2025-09-11 16:38:57 +0200

    [#92] Delete unsued interfaces and mocks

--- controllers/interfaces.go
+++ controllers/interfaces.go
@@ -3,16 +3,12 @@ package controllers
 import (
 	"context"
 
-	"github.com/cloudogu/k8s-dogu-operator/v3/api/ecoSystem"
 	libconfig "github.com/cloudogu/k8s-registry-lib/config"
 	"github.com/cloudogu/k8s-registry-lib/repository"
 	"github.com/cloudogu/k8s-service-discovery/v2/internal/types"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	appsv1 "k8s.io/client-go/kubernetes/typed/apps/v1"
 	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
-	netv1 "k8s.io/client-go/kubernetes/typed/networking/v1"
 	"k8s.io/client-go/tools/record"
 )
 
@@ -42,12 +38,6 @@ type certificateSynchronizer interface {
 	Synchronize(ctx context.Context) error
 }
 
-//nolint:unused
-//goland:noinspection GoUnusedType
-type doguInterface interface {
-	ecoSystem.DoguInterface
-}
-
 type AlternativeFQDNRedirector interface {
 	RedirectAlternativeFQDN(ctx context.Context, namespace string, redirectObjectName string, fqdn string, altFQDNList []types.AlternativeFQDN, setOwner func(targetObject metav1.Object) error) error
 }
@@ -64,42 +54,6 @@ type IngressController interface {
 	AlternativeFQDNRedirector
 	IngressControllerSelector
 	PortExposer
-}
-
-//nolint:unused
-//goland:noinspection GoUnusedType
-type ingressInterface interface {
-	netv1.IngressInterface
-}
-
-//nolint:unused
-//goland:noinspection GoUnusedType
-type clientSetInterface interface {
-	kubernetes.Interface
-}
-
-//nolint:unused
-//goland:noinspection GoUnusedType
-type appsv1Interface interface {
-	appsv1.AppsV1Interface
-}
-
-//nolint:unused
-//goland:noinspection GoUnusedType
-type netInterface interface {
-	netv1.NetworkingV1Interface
-}
-
-//nolint:unused
-//goland:noinspection GoUnusedType
-type deploymentInterface interface {
-	appsv1.DeploymentInterface
-}
-
-//nolint:unused
-//goland:noinspection GoUnusedType
-type ingressClassInterface interface {
-	netv1.IngressClassInterface
 }
 
 type secretClient interface {
