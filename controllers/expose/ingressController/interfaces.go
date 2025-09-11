9776758e7939a9db57ecd0306ab1f4a67bb708e1
Nico Franzeck <nico.franzeck@cloudogu.com>
2025-09-11 15:05:49 +0200

    [#92] Return only functions needed in other packages

--- controllers/expose/ingressController/interfaces.go
+++ controllers/expose/ingressController/interfaces.go
@@ -3,7 +3,6 @@ package ingressController
 import (
 	"context"
 
-	"github.com/cloudogu/k8s-service-discovery/v2/controllers/util"
 	"github.com/cloudogu/k8s-service-discovery/v2/internal/types"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
@@ -18,19 +17,12 @@ type ingressInterface interface {
 	netv1.IngressInterface
 }
 
-// tcpUpdServiceExposer is used to expose non http services.
-type tcpUpdServiceExposer interface {
-	// ExposeOrUpdateExposedPorts adds or updates the exposing of the exposed ports in the dogu from the cluster. These are typically
-	// entries in a configmap.
-	ExposeOrUpdateExposedPorts(ctx context.Context, namespace string, targetServiceName string, exposedPorts util.ExposedPorts) error
-	// DeleteExposedPorts removes the exposing of the exposed ports in the dogu from the cluster. These are typically
-	// entries in a configmap.
-	DeleteExposedPorts(ctx context.Context, namespace string, targetServiceName string) error
-	ExposePorts(ctx context.Context, namespace string, exposedPorts types.ExposedPorts, owner *metav1.OwnerReference) error
-}
-
 type AlternativeFQDNRedirector interface {
 	RedirectAlternativeFQDN(ctx context.Context, namespace string, redirectObjectName string, fqdn string, altFQDNList []types.AlternativeFQDN, setOwner func(targetObject metav1.Object) error) error
+}
+
+type PortExposer interface {
+	ExposePorts(ctx context.Context, namespace string, exposedPorts types.ExposedPorts, owner *metav1.OwnerReference) error
 }
 
 type IngressController interface {
@@ -39,6 +31,6 @@ type IngressController interface {
 	GetRewriteAnnotationKey() string
 	GetUseRegexKey() string
 	GetSelector() map[string]string
-	tcpUpdServiceExposer
 	AlternativeFQDNRedirector
+	PortExposer
 }
