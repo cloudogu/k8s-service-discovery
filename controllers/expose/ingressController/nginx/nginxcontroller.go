fdc18b29b8daa904ecdda48ea23adb620c98ba21
Nico Franzeck <nico.franzeck@cloudogu.com>
2025-09-12 15:25:21 +0200

    [#92] Add support for ces-gateway

--- controllers/expose/ingressController/nginx/nginxcontroller.go
+++ controllers/expose/ingressController/nginx/nginxcontroller.go
@@ -8,10 +8,38 @@ const (
 	ingressRewriteTargetAnnotation = "nginx.ingress.kubernetes.io/rewrite-target"
 	ingressUseRegexAnnotation      = "nginx.ingress.kubernetes.io/use-regex"
 	nginxIngressControllerSpec     = "k8s.io/nginx-ingress"
-	nginxIngressControllerName     = "nginx-ingress"
+
+	IngressControllerName = "nginx-ingress"
+	GatewayControllerName = "k8s-ces-gateway"
+
+	componentLabelKey = "k8s.cloudogu.com/component.name"
 )
 
+type controllerType uint8
+
+func (c controllerType) String() string {
+	switch c {
+	case gateway:
+		return GatewayControllerName
+	case ingress:
+		return IngressControllerName
+	}
+
+	return "unknown"
+}
+
+const (
+	gateway controllerType = iota
+	ingress
+)
+
+var selectorMap = map[controllerType]map[string]string{
+	gateway: {componentLabelKey: GatewayControllerName},
+	ingress: {k8sv2.DoguLabelName: IngressControllerName},
+}
+
 type IngressController struct {
+	controllerType
 	*PortExposer
 	*IngressRedirector
 }
@@ -20,6 +48,7 @@ type IngressControllerDependencies struct {
 	ConfigMapInterface configMapInterface
 	IngressInterface   ingressInterface
 	IngressClassName   string
+	ControllerType     string
 }
 
 func NewNginxController(deps IngressControllerDependencies) *IngressController {
@@ -31,11 +60,12 @@ func NewNginxController(deps IngressControllerDependencies) *IngressController {
 			ingressClassName: deps.IngressClassName,
 			ingressInterface: deps.IngressInterface,
 		},
+		controllerType: mapStringToControllerType(deps.ControllerType),
 	}
 }
 
 func (c *IngressController) GetName() string {
-	return nginxIngressControllerName
+	return c.controllerType.String()
 }
 
 func (c *IngressController) GetControllerSpec() string {
@@ -51,7 +81,16 @@ func (c *IngressController) GetUseRegexKey() string {
 }
 
 func (c *IngressController) GetSelector() map[string]string {
-	return map[string]string{
-		k8sv2.DoguLabelName: nginxIngressControllerName,
+	return selectorMap[c.controllerType]
+}
+
+func mapStringToControllerType(s string) controllerType {
+	switch s {
+	case GatewayControllerName:
+		return gateway
+	case IngressControllerName:
+		return ingress
+	default:
+		return 0
 	}
 }
--- controllers/expose/ingressController/nginx/nginxcontroller_test.go
+++ controllers/expose/ingressController/nginx/nginxcontroller_test.go
@@ -3,7 +3,6 @@ package nginx
 import (
 	"testing"
 
-	k8sv2 "github.com/cloudogu/k8s-dogu-operator/v3/api/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -12,29 +11,75 @@ func TestNewNginxController(t *testing.T) {
 	cfgMock := newMockConfigMapInterface(t)
 	ingressMock := newMockIngressInterface(t)
 
-	ctrl := NewNginxController(IngressControllerDependencies{
-		ConfigMapInterface: cfgMock,
-		IngressInterface:   ingressMock,
-		IngressClassName:   "test",
-	})
+	tests := []struct {
+		name              string
+		inControllerType  string
+		expControllerType controllerType
+	}{
+		{
+			name:              "default",
+			inControllerType:  "",
+			expControllerType: gateway,
+		},
+		{
+			name:              "k8s-ces-gateway component",
+			inControllerType:  GatewayControllerName,
+			expControllerType: gateway,
+		},
+		{
+			name:              "nginx-ingress dogu",
+			inControllerType:  IngressControllerName,
+			expControllerType: ingress,
+		},
+	}
 
-	require.NotNil(t, ctrl)
-	require.NotNil(t, ctrl.configMapInterface)
-	require.NotNil(t, ctrl.ingressInterface)
-	require.Equal(t, "test", ctrl.ingressClassName)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := NewNginxController(IngressControllerDependencies{
+				ConfigMapInterface: cfgMock,
+				IngressInterface:   ingressMock,
+				IngressClassName:   "test",
+				ControllerType:     tt.inControllerType,
+			})
+
+			require.NotNil(t, ctrl)
+			require.NotNil(t, ctrl.configMapInterface)
+			require.NotNil(t, ctrl.ingressInterface)
+			require.Equal(t, "test", ctrl.ingressClassName)
+			require.Equal(t, tt.expControllerType, ctrl.controllerType)
+		})
+	}
 }
 
 func Test_controller_GetName(t *testing.T) {
-	t.Run("success", func(t *testing.T) {
-		// given
-		sut := &IngressController{}
+	tests := []struct {
+		name             string
+		inControllerType controllerType
+		expName          string
+	}{
+		{
+			name:             "default",
+			inControllerType: 0,
+			expName:          GatewayControllerName,
+		},
+		{
+			name:             "k8s-ces-gateway component",
+			inControllerType: gateway,
+			expName:          GatewayControllerName,
+		},
+		{
+			name:             "nginx-ingress dogu",
+			inControllerType: ingress,
+			expName:          IngressControllerName,
+		},
+	}
 
-		// when
-		name := sut.GetName()
-
-		// then
-		require.Equal(t, "nginx-ingress", name)
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := &IngressController{controllerType: tt.inControllerType}
+			assert.Equal(t, tt.expName, ctrl.GetName())
+		})
+	}
 }
 
 func Test_controller_GetControllerSpec(t *testing.T) {
@@ -77,10 +122,68 @@ func Test_controller_GetUseRegexKey(t *testing.T) {
 }
 
 func TestIngressController_GetSelector(t *testing.T) {
-	sut := IngressController{}
-	exp := map[string]string{
-		k8sv2.DoguLabelName: nginxIngressControllerName,
+	tests := []struct {
+		name             string
+		inControllerType controllerType
+		expSelector      map[string]string
+	}{
+		{
+			name:             "default",
+			inControllerType: 0,
+			expSelector:      selectorMap[gateway],
+		},
+		{
+			name:             "k8s-ces-gateway component",
+			inControllerType: gateway,
+			expSelector:      selectorMap[gateway],
+		},
+		{
+			name:             "nginx-ingress dogu",
+			inControllerType: ingress,
+			expSelector:      selectorMap[ingress],
+		},
 	}
 
-	assert.Equal(t, exp, sut.GetSelector())
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := &IngressController{controllerType: tt.inControllerType}
+			assert.Equal(t, tt.expSelector, ctrl.GetSelector())
+		})
+	}
+}
+
+func Test_controllerType_String(t *testing.T) {
+	tests := []struct {
+		name             string
+		inControllerType controllerType
+		expName          string
+	}{
+		{
+			name:             "default",
+			inControllerType: 0,
+			expName:          GatewayControllerName,
+		},
+		{
+			name:             "k8s-ces-gateway component",
+			inControllerType: gateway,
+			expName:          GatewayControllerName,
+		},
+		{
+			name:             "nginx-ingress dogu",
+			inControllerType: ingress,
+			expName:          IngressControllerName,
+		},
+		{
+			name:             "invalid",
+			inControllerType: 100,
+			expName:          "unknown",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := &IngressController{controllerType: tt.inControllerType}
+			assert.Equal(t, tt.expName, ctrl.String())
+		})
+	}
 }
