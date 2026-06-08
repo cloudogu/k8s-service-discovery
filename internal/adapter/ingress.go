01aaba7d1efa49652b44a13e3fa2eac26a023d07
Nico Franzeck <nico.franzeck@cloudogu.com>
2026-06-08 13:43:32 +0200

    [#105] Remove unused ingressClass from ingress.go

--- internal/adapter/ingress.go
+++ internal/adapter/ingress.go
@@ -41,11 +41,10 @@ type ingressController interface {
 // backend, then delegates the actual ingress materialization to an
 // ingressController.
 type Ingress struct {
-	ingressClass string
-	client       client.Client
-	dogu         doguAdapter
-	maintenance  maintenanceAdapter
-	controller   ingressController
+	client      client.Client
+	dogu        doguAdapter
+	maintenance maintenanceAdapter
+	controller  ingressController
 }
 
 // GetOwnableTypes delegates to the underlying ingressController so
